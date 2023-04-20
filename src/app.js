677b2a710ecbbe871bd522119572bbcc1b86c330
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-04-20 22:06:09 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,12 +11,28 @@ const server = http.createServer((req, res) => {
       const obj = JSON.parse(chunks)
       const value1 = obj.num1;
       const value2 = obj.num2;
+      if (Number.isInteger(value1) && Number.isInteger(value2)) {
+        if (value1 <= 0) {
+          res.writeHead(404, {'Content-type': 'text/plain' });
+          res.end('this is not a valid expression');
+        }
+        else if (value2 < 0) {
+          res.writeHead(404, {'Content-type': 'text/plain' });
+          res.end('this is not a valid expression');
+        }
+        else {
+          const result = Math.pow(num1, num2);
+          res.writeHead(200, {'Content-type':'text/plain'});
+          res.end(`The result is ${result}`);
+        }
 
-      // Write code here to calculate power of a number
-      
+      } else {
+        res.writeHead(400, { 'Content-type': 'text/plain' });
+        res.end('this is a bad request');
+      }
+
     });
-    }
+  }
 });
 
 module.exports = server;
-      
